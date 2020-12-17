410cc1efe0e9d90dc04e00fa4b9cea04c8cdfb4a
dangyuuki123 <63136168+dangyuuki123@users.noreply.github.com>
2020-12-17 19:52:52 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+preprocessing = {
+
+    'data_dir': '/home/u52660/data/25hours/',
+    'window_size': 20,
+    'step_size': 10,
+
+}
+
+model = {
+
+    'verbose': 1,
+
+    'conv_channels': [32 , 32 , 64 , 128],
+    'conv_filters': [13 , 11, 7 , 5 ],
+    'conv_strides': [1,1,1,1],
+
+    'rnn_units': [1024 , 1024,1024,1024 , 1024 ,1024],
+    'bidirectional_rnn': True,
+
+    'future_context': 2,
+
+    'use_bn': True,
+
+    'learning_rate': 0.001
+
+}
+
+training = {
+
+    'tensorboard': False,
+    'log_dir': './logs',
+
+    'batch_size': 64,
+    'epochs': 300,
+    'validation_size': 0.2
+
+}
